23c9f67be04550de881c10b6b2f0f76633843f7c
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 18:10:12 +0300

    Файл локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'Media types',
+    '{description}' => 'Media types',
+    '{permissions}' => [
+        'any'  => ['Full access', 'Viewing and making changes to media types'],
+        'view' => ['View', 'Viewing media types'],
+        'read' => ['Read', 'Reading media types']
+    ],
+
+    // Grid: контекстное меню записи
+    'Edit record' => 'Edit record',
+    // Grid: столбцы
+    'Name' => 'Name',
+    'Type' => 'Type',
+    'Icon' => 'Icon',
+
+    // Form
+    '{form.title}' => 'Add media types',
+    '{form.titleTpl}' => 'Edit media types "{name}"'
+];
