23e0f68d9c13adcf29bb77b247b80f87738b509e
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 18:09:31 +0300

    Extension

---
+++ src/Extension.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Расширение модуля веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Backend\References\MediaTypes;
+
+/**
+ * Расширение "Тип медиаданных".
+ * 
+ * Расширение принадлежит модулю "Справочники".
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Backend\References\MediaTypes
+ * @since 1.0
+ */
+class Extension extends \Ge\Panel\Extension\Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public string $id = 'rg.be.references.media_types';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $defaultController = 'grid';
+}
