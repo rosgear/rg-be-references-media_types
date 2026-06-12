604344ca0c88b2fcb500d7893b96be16b873b5ed
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 18:09:58 +0300

    Файл конфигурации версии расширения

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии расширения.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'Media types',
+    'description' => 'Media types',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-be-references-media_types/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
